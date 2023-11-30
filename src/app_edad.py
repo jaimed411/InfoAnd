7d1600ba83a26676dc060eea20af9653df26f92b
Jaime Dodero <131529197+jaimed411@users.noreply.github.com>
2023-11-30 18:04:28 +0000

    .

--- src/app_edad.py
+++ src/app_edad.py
@@ -3,13 +3,13 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 import joblib
 
-
 # Cargar el modelo entrenado
-model = joblib.load("../data/modelo_edad.joblib")
+modelo_ruta = '/workspaces/InfoAnd/data/modelo_edad.joblib'
+model = joblib.load(modelo_ruta)
 
 # Cargar los conjuntos de entrenamiento y prueba
-X_train_ruta = '../EDAD/X_train.csv'
-y_test_ruta = '../EDAD/y_test.csv'
+X_train_ruta = '/workspaces/InfoAnd/EDAD/X_train.csv'
+y_test_ruta = '/workspaces/InfoAnd/EDAD/y_test.csv'
 X_train = pd.read_csv(X_train_ruta)
 y_test = pd.read_csv(y_test_ruta)
 
