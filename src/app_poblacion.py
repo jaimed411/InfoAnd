2ec61cdc88e26ab31c511d90b761970adbd594cf
Jaime Dodero <131529197+jaimed411@users.noreply.github.com>
2023-11-30 19:03:18 +0000

    .

--- src/app_poblacion.py
+++ src/app_poblacion.py
@@ -4,12 +4,12 @@ from sklearn.linear_model import LinearRegression
 import joblib
 
 # Cargar el modelo entrenado
-modelo_ruta = '/workspaces/InfoAnd/POBLACION/modelo_poblacion.joblib'
+modelo_ruta = '../data/modelo_poblacion.joblib'
 model = joblib.load(modelo_ruta)
 
 # Cargar los conjuntos de entrenamiento y prueba
-X_train_ruta = '/workspaces/InfoAnd/POBLACION/X_train.csv'
-y_test_ruta = '/workspaces/InfoAnd/POBLACION/y_test.csv'
+X_train_ruta = '../POBLACION/X_train.csv'
+y_test_ruta = '../POBLACION/y_test.csv'
 X_train = pd.read_csv(X_train_ruta)
 y_test = pd.read_csv(y_test_ruta)
 
