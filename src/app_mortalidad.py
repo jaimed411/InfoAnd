41cb04fbd5bf9900c0bdeada22a24813be918b51
Jaime Dodero <131529197+jaimed411@users.noreply.github.com>
2023-11-30 18:45:46 +0000

    .

--- src/app_mortalidad.py
+++ src/app_mortalidad.py
@@ -4,12 +4,12 @@ from sklearn.linear_model import LinearRegression
 import joblib
 
 # Cargar el modelo entrenado
-modelo_ruta = '/workspaces/InfoAnd/MORTALIDAD/modelo_mortalidad.joblib'
+modelo_ruta = '../data/modelo_mortalidad.joblib'
 model = joblib.load(modelo_ruta)
 
 # Cargar los conjuntos de entrenamiento y prueba
-X_train_ruta = '/workspaces/InfoAnd/MORTALIDAD/X_train.csv'
-y_test_ruta = '/workspaces/InfoAnd/MORTALIDAD/y_test.csv'
+X_train_ruta = '../MORTALIDAD/X_train.csv'
+y_test_ruta = '../MORTALIDAD/y_test.csv'
 X_train = pd.read_csv(X_train_ruta)
 y_test = pd.read_csv(y_test_ruta)
 
